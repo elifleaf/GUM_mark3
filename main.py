cc2d8972fcc7c5055ab79c15fc9fd5cd1570f786
elifleaf <elif.ertekin@gmail.com>
2018-05-18 09:44:54 -0500

    merging ...

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ import sys
 ############################
 ## Some notes from Elif's Modifications to Parameter Fitting
 ##
-## *) I removed NiMnIn_Au from Brian's data directory, it was not tetragonal and doesn't work
-## *) it is finding some non-duplicate duplicates
-## *) first task should be to separate out the rules sums calculations from the post-processing -- first make the pp file, then calculate sums
-##
 ## *) As implemented right now,
 ##      several parts of this code assume that the VASP POSCAR/CONTCAR always enter Ni,Mn,In in the
 ##      same order.  You will be in trouble on many levels if this is not actually the case.
@@ -24,20 +20,15 @@ import sys
 ##      information necessary to do the fitting and to run MC simulations.  It is carrying around
 ##      a lot of information we don't need.  Cleaner to leave it behind when its not needed.
 ##
-## *) we should make an option to just read cluster and j sums from summary rather than regenerating each time
-##
 ## *) Look at m_structure.py line 86 for minor question.
-##
-## *) need to assess degree of overfitting in model using cross-validation in sklearn. Can we improve the model?
 ##
 ## *) When doing the fitting, is selecting an intercept a problem? cluster expansion model inherently
 ##      expects non-centered data?  Otherwise it spends it energy pulling out the linear dependence on composition.
 ##      Note: fitting is currently modified so intercept is not fit, need to change cfp.ridge_simple if we want
 ##      to include it.  But I think we do not.
-##
-## *) SEE if we can recover Brian's fit. Then do changes in line 100
+##################################################
 
-aust_tol = 0.01
+aust_tol = 0.02
 spin_style = ['threshold','threshold','threshold']  # options for spin_tol. Assuming [Ni Mn In]. choose 'threshold' or 'factor'
 spin_tol = [0.05,2,0]                                # insert spin parameters here, this assumes [Ni Mn In ]
 species = ['Ni','Mn','In']                          # this is the order that the post-processed data is reported, NEEDS TO BE Heusler format Ni2MnIn, Ni2FeGa.
@@ -53,12 +44,8 @@ j_file = './J_Rules'                                # heisenberg rules
 vasp_data_exists = True                            # should be False if I want to regenrate files
 vasp_pp_exists = False                              # postprocessing of VASP results or no?
 Cluster_rules_exist = True                          # define cluster rules
-J_rules_exist = True                                # define heisenberg rules
-#Js_exist = True                                     # results of fitting model
-
-# summarize VASP data
-if vasp_data_exists is False:                    # will make summary of VASP results if it doesn't already exist
-    cvs.import_vasp(root_dir, vasp_data_file,species)
+J_rules_exist = True                                # define heisenberg rules                                  # results of fitting model
+Fitting_params_exist = True
 
 # write Cluster and J rules file if doesn't exist, then read the rules
 if Cluster_rules_exist is False:                    # writes cluster rules if doesn't already exist
@@ -67,36 +54,41 @@ if J_rules_exist is False:                          # writes j_rules if doesn't 
     cmr.write_j_rules(j_file)
 Cluster_rules = cmr.read_cluster_rules(cluster_file)
 J_rules = cmr.read_j_rules(j_file)
+if Fitting_params_exist == False:
+    # summarize VASP data
+    if vasp_data_exists is False:                    # will make summary of VASP results if it doesn't already exist
+        cvs.import_vasp(root_dir, vasp_data_file,species)
 
-# Read the VASP datafile and initialize a structure object for each
-# postprocess according to user selected parameters above and the cluster and j rules
-# calculation of sums and checking for duplicates occurs in here now
-# if a given structure is considered a duplicate then it is not added to the structure_list
-M_structures = ppv.generate_m_structure(vasp_data_file, len(Cluster_rules), len(J_rules), aust_tol, spin_style, spin_tol, Cluster_rules, J_rules)
+    # Read the VASP datafile and initialize a structure object for each
+    # postprocess according to user selected parameters above and the cluster and j rules
+    # calculation of sums and checking for duplicates occurs in here now
+    # if a given structure is considered a duplicate then it is not added to the structure_list
+    M_structures = ppv.generate_m_structure(vasp_data_file, len(Cluster_rules), len(J_rules), aust_tol, spin_style, spin_tol, Cluster_rules, J_rules)
 
-ppv.write_structures_processedvasp(M_structures,vasp_data_file_pp)
-ppv.summarize_classification(M_structures)
-# Seems like there should be the option to read the sums from the
-# summary_fitting_structures file to avoid doing this summing each time.
-warning_threshold = 0.5
-ppv.summarize_fitting_structures(M_structures,warning_threshold)
+    ppv.write_structures_processedvasp(M_structures,vasp_data_file_pp)
+    ppv.summarize_classification(M_structures)
+    warning_threshold = 0.5
+    ppv.summarize_fitting_structures(M_structures,warning_threshold)
 
-## Ridge Regression Fitting with Regularization
-Js,intercept = cfp.ridge_simple(M_structures,1,Cluster_rules,J_rules)
-cfp.write_fitting_parameters(M_structures, Cluster_rules, J_rules, Js, intercept, 200)
-cfp.plot_data3(M_structures,Cluster_rules,J_rules,Js,intercept,200)
+    ## Ridge Regression Fitting with Regularization
+    Js,intercept = cfp.ridge_simple(M_structures,1,Cluster_rules,J_rules)
+    cfp.write_fitting_parameters(M_structures, Cluster_rules, J_rules, Js, intercept, 200)
+    cfp.plot_data3(M_structures,Cluster_rules,J_rules,Js,intercept,200)
 
-#print('#######################\n')
-##print(cp.CV_score(Js,M_structures))
-##print(cp.CV_score2(M_structures))
-#print('#######################\n')
-##--------------------------------------------------------------#
-
-
-
-
-
-
+else:
+    paramiters_file = open('FittingParameters','r')
+    lines = paramiters_file.readlines()
+    reading_params = True
+    line_index = 1
+    Js = []
+    while reading_params == True:
+        line = lines[line_index]
+        if 'Actual Energy' in line or line.split() == []:
+            reading_params = False
+        else:
+            line = line.split()
+            Js.append(float(line[2]))
+            line_index += 1
 
 
 ##--------------------------------------------------------------#
@@ -112,22 +104,22 @@ y_pts = 2 #|- Dimensions of the mc_supercellObj (simulation supercell)
 z_pts = 4 #|
 phase_init = 'mart' # initial phase configuration
 spin_init = 'rand' # initial spin configuration
-species_init = 'ordered'
-num_passes = 15 # number of cluster/wolf moves done
-num_sub_passes = 20 # number of spin/species flips done per cluster/wolf move
+species_init = 'rand'    # ordered or rand
+num_passes = 400 # number of cluster/wolf moves done
+num_sub_passes = 30 # number of spin/species flips done per cluster/wolf move
 Temp0 = 100 # initial temperature in K
-TempF = 600 # final temperature in K
+TempF = 210 # final temperature in K
 Temp_inc = 5 # temperature increase per pass in K
 
 ## Initialize an array of atoms with ms.mc_supercellObj(size,species,composition)
 ## size is (x,y,z)dimensions, species is types of atoms allowed (0=Ni,1=Mn,2=In)
 ## composition is number of each atom (#Ni,#Mn,#In)
-## lattice = ms.mc_supercellObj((x_pts,y_pts,z_pts),(0,1,2),[8,8,0],phase_init,spin_init,species_init)
+lattice = ms.mc_supercellObj((x_pts,y_pts,z_pts),(0,1,2),[8,8,0],phase_init,spin_init,species_init)
 #sys.setrecursionlimit(lattice.num_sites+2)
 
 ## To actually run the simulation use
 ## mc.run_montecarlo(reference_to_atom_array,number_of_passes,starting_temp, BEG_rules,Cluster_rules,J_rules,plot_figs=TRUE)
 ## BEG_rules,Cluster_rules,J_rules are objects that determine when and how the fitted parameters are applied
-## print("Beginning MonteCarlo\n")
-## mc.run_WA_MCA(lattice,num_passes,num_sub_passes,Temp0,Temp_inc,TempF,Cluster_rules,J_rules,Js,do_figs=True)
-##mc2.run_WA_MCA(lattice,num_passes,num_sub_passes,Temp0,Temp_inc,TempF,Cluster_rules,J_rules,Js,do_figs=True)
+print("Beginning MonteCarlo\n")
+mc2.run_WA_MCA(lattice,num_passes,num_sub_passes,Temp0,Temp_inc,TempF,Cluster_rules,J_rules,Js,do_figs=True)
+
